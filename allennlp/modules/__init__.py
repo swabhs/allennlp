58c06e996c46448829bf53fcdf079fa1bf973021
swabhs <swabha.swayamdipta@gmail.com>
2018-10-18 13:23:33 -0700

    implemented tests passing

--- allennlp/modules/__init__.py
+++ allennlp/modules/__init__.py
@@ -23,6 +23,7 @@ from allennlp.modules.matrix_attention import MatrixAttention
 from allennlp.modules.attention import Attention
 from allennlp.modules.input_variational_dropout import InputVariationalDropout
 from allennlp.modules.bimpm_matching import BiMpmMatching
+from allennlp.modules.semi_markov_crf import SemiMarkovConditionalRandomField
 
 def SpanPruner(*args, **kwargs) -> Pruner:  # pylint: disable=invalid-name
     import warnings
