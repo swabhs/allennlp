299fe4679260b044d5d78cb317d6437e98a9d4cf
swabhs <swabha.swayamdipta@gmail.com>
2019-01-15 16:04:47 -0800

    added test

--- allennlp/tests/data/token_indexers/chunky_elmo_indexer_test.py
+++ allennlp/tests/data/token_indexers/chunky_elmo_indexer_test.py
@@ -3,9 +3,18 @@ from collections import defaultdict
 
 from allennlp.common.testing import AllenNlpTestCase
 from allennlp.data import Token, Vocabulary
+from allennlp.chunky.chunky_elmo_indexer import ChunkyElmoIndexer
 
 
 class TestChunkyElmoIndexer(AllenNlpTestCase):
     def setUp(self):
         super(TestChunkyElmoIndexer, self).setUp()
-        
+
+        indexer = ChunkyElmoIndexer()
+        sentence = "I have a good dog ."
+        indices = indexer.tokens_to_indices([Token(word) for word in sentence.split()])
+
+        assert False
+
+
+
