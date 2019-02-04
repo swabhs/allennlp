a66cd4ceab01904df6c3729fee8b29c110211e7f
swabhs <swabha.swayamdipta@gmail.com>
2019-02-03 21:41:03 -0800

    test, first pass

---
+++ allennlp/tests/fixtures/segmental_language_model/experiment.jsonnet
@@ -0,0 +1,90 @@
+{
+  "dataset_reader":  {
+    "type": "segmental_conll2000",
+    "token_indexers": {
+      "tokens": {
+        "type": "single_id"
+      },
+      "token_characters": {
+        "type": "elmo_characters"
+      }
+  },
+  "train_data_path": "allennlp/tests/fixtures/data/chunks_bioul.conll",
+  "vocabulary": {
+      "tokens_to_add": {
+          "tokens": ["<S>", "</S>"],
+          "token_characters": ["<>/S"]
+      },
+  },
+  "model": {
+    "type": "segmental_language_model",
+    "bidirectional": true,
+    "num_samples": 8192,
+    "sparse_embeddings": true,
+    "text_field_embedder": {
+      "allow_unmatched_keys": true,
+      "token_embedders": {
+        "token_characters": {
+            "type": "character_encoding",
+            "embedding": {
+                "num_embeddings": 262,
+                "embedding_dim": 16
+            },
+            "encoder": {
+                "type": "cnn-highway",
+                "activation": "relu",
+                "embedding_dim": 16,
+                "filters": [
+                    [1, 32],
+                    [2, 32],
+                    [3, 64]],
+                "num_highway": 2,
+                "projection_dim": 512,
+                "projection_location": "after_highway",
+                "do_layer_norm": true
+            }
+        }
+      }
+    },
+    "dropout": 0.1,
+    "contextualizer": {
+        "type": "bidirectional_language_model_transformer",
+        "input_dim": 5,
+        "hidden_dim": 10,
+        "num_layers": 6,
+        "dropout": 0.1,
+        "input_dropout": 0.1
+    },
+    "forward_segmental_contextualizer": {
+      "type": "bidirectional_language_model_transformer",
+      "input_dim": 5,
+      "hidden_dim": 16,
+      "input_dropout": 0.1,
+      "num_layers": 2,
+      "direction": "forward"
+    },
+    "backward_segmental_contextualizer": {
+      "type": "bidirectional_language_model_transformer",
+      "input_dim": 5,
+      "hidden_dim": 16,
+      "input_dropout": 0.1,
+      "num_layers": 2,
+      "direction": "backward"
+    },
+    "softmax_projection_dim": 13,
+    "label_feature_dim": 14
+  },
+  "iterator": {
+    "type": "basic",
+    "batch_size": 32,
+  },
+  "trainer": {
+    "num_epochs": 10,
+    "cuda_device" : -1,
+    "optimizer": {
+      "type": "sgd",
+      "lr": 0.01
+    },
+    "log_batch_size_period": 1
+  }
+}
---
+++ allennlp/tests/models/segmental_language_model_test.py
@@ -0,0 +1,94 @@
+# pylint: disable=invalid-name,arguments-differ,abstract-method
+import numpy as np
+import pytest
+
+from allennlp.common.testing import ModelTestCase
+from allennlp.common.checks import ConfigurationError
+from allennlp.common.params import Params
+from allennlp.models import Model
+from allennlp.models.segmental_language_model import SegmentalLanguageModel
+
+
+class TestSegmentalLanguageModel(ModelTestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.expected_embedding_shape = (3, 21, 40)
+        self.set_up_model(self.FIXTURES_ROOT / 'segmental_language_model' / 'experiment.jsonnet',
+                          self.FIXTURES_ROOT / 'data' / 'chunks_bioul.conll')
+
+    # pylint: disable=no-member
+    def test_unidirectional_language_model_can_train_save_and_load(self):
+        self.ensure_model_can_train_save_and_load(self.param_file)
+
+    def test_batch_predictions_are_consistent(self):
+        self.ensure_batch_predictions_are_consistent(keys_to_ignore=["batch_weight"])
+
+    def test_forward_pass_runs_correctly(self):
+        training_tensors = self.dataset.as_tensor_dict()
+        result = self.model(**training_tensors)
+
+        assert set(result) == {"loss", "forward_loss", "backward_loss", "lm_embeddings",
+                               "noncontextual_token_embeddings", "mask", "batch_weight"}
+
+        # The model should preserve the BOS / EOS tokens.
+        embeddings = result["lm_embeddings"]
+        assert tuple(embeddings.shape) == self.expected_embedding_shape
+
+        loss = result["loss"].item()
+        forward_loss = result["forward_loss"].item()
+        if self.bidirectional:
+            backward_loss = result["backward_loss"].item()
+            np.testing.assert_almost_equal(loss, (forward_loss + backward_loss) / 2,
+                                           decimal=3)
+        else:
+            np.testing.assert_almost_equal(loss, forward_loss, decimal=3)
+            assert result["backward_loss"] is None
+
+    def test_get_gathered_embeddings_shorter(self):
+        embeddings = torch.Tensor([
+            [[0.0842, 0.8478], [0.9810, 0.3842], [0.3279, 0.7760], [0.8819, 0.7125]],
+            [[0.0822, 0.6478], [0.8478, 0.5672], [0.8910, 0.0000], [0.0000, 0.0000]],
+            [[0.0312, 0.6372], [0.8983, 0.4015], [0.0000, 0.0000], [0.0000, 0.0000]]
+            ])
+        indices = torch.LongTensor([[[0], [2], [3]],
+                                    [[0], [1], [2]],
+                                    [[0], [1], [-1]]
+                                    ])
+
+        expected_gathered = torch.Tensor([
+            [[0.0842, 0.8478], [0.3279, 0.7760], [0.8819, 0.7125]],
+            [[0.0822, 0.6478], [0.8478, 0.5672], [0.8910, 0.0000]],
+            [[0.0312, 0.6372], [0.8983, 0.4015], [0.0312, 0.6372]]
+            ])
+        expected_mask = torch.LongTensor([[1, 1, 1],
+                                          [1, 1, 1],
+                                          [1, 1, 0]
+                                          ])
+
+        gathered, mask = SegmentalLanguageModel._get_gathered_embeddings(embeddings, indices)
+
+        self.assertTrue(bool((expected_gathered == gathered).all().item()))
+        self.assertTrue(bool((expected_mask == mask).all().item()))
+
+    def test_get_gathered_embeddings_longer(self):
+        embeddings = torch.Tensor([
+            [[0.0842, 0.8478], [0.9810, 0.3842], [0.3279, 0.7760], [0.8819, 0.7125]],
+            [[0.0822, 0.6478], [0.8478, 0.5672], [0.8910, 0.0000], [0.0000, 0.0000]],
+            [[0.0312, 0.6372], [0.8983, 0.4015], [0.0000, 0.0000], [0.0000, 0.0000]]
+            ])
+        indices = torch.LongTensor([[[0], [0], [1], [1], [1], [2], [2], [3]],
+                                    [[0], [1], [2], [2], [2], [3], [-1], [-1]],
+                                    [[0], [0], [0], [0], [1], [-1], [-1], [-1]]
+                                    ])
+
+        expected_gathered = torch.Tensor([
+            [[0.0842, 0.8478], [0.0842, 0.8478], [0.9810, 0.3842], [0.9810, 0.3842], [0.9810, 0.3842], [0.3279, 0.7760], [0.3279, 0.7760], [0.8819, 0.7125]],
+            [[0.0822, 0.6478], [0.8478, 0.5672], [0.8910, 0.0000], [0.8910, 0.0000], [0.8910, 0.0000], [0.0000, 0.0000], [0.0822, 0.6478], [0.0822, 0.6478]],
+            [[0.0312, 0.6372], [0.0312, 0.6372], [0.0312, 0.6372], [0.0312, 0.6372], [0.8983, 0.4015], [0.0312, 0.6372], [0.0312, 0.6372], [0.0312, 0.6372],]
+            ])
+
+        gathered, _ = SegmentalLanguageModel._get_gathered_embeddings(embeddings, indices)
+        self.assertTrue(bool((expected_gathered == gathered).all().item()))
+
+        self.assertTrue(bool((expected_gathered == gathered).all().item()))
