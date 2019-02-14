af86237ac9d966152a2324ffe1ff38855df9214e
swabhs <swabha.swayamdipta@gmail.com>
2019-02-14 11:18:00 -0800

    bugfix

--- bin/average_random_seeds.py
+++ bin/average_random_seeds.py
@@ -28,19 +28,19 @@ if __name__ == '__main__':
 
         val_metric = results['best_validation_' + metric]
         val.append(val_metric)
-        try:
-            test_metric = results['test_' + metric]
-            test.append(test_metric)
-        except KeyError:
-            pass
+        # try:
+        #     test_metric = results['test_' + metric]
+        #     test.append(test_metric)
+        # except KeyError:
+        #     pass
 
         try:
-            with open(os.path.join(seed_dir, 'metrics.json'), 'r') as fin:
+            with open(os.path.join(seed_dir, 'evaluation_metrics.json'), 'r') as fin:
                 eval_results = json.load(fin)
 
             test_metric = eval_results[metric]
             test.append(test_metric)
-        except (KeyError, OSError):
+        except (KeyError, OSError) as e:
             pass
 
 
@@ -48,17 +48,18 @@ if __name__ == '__main__':
     for i, metrics in enumerate([val, test]):
         if i == 0:
             print("VALIDATION")
+        elif i==1 and len(metrics) == 0:
+            print("NO TEST")
+            continue
         else:
             print("TEST")
-        if len(metrics) == 0:
-            print("NO TEST")
-        else:
-            mm = np.array(metrics) * 100
-            m = np.mean(mm)
-            s = np.std(mm)
-            mn = np.min(mm)
-            mx = np.max(mm)
-            fmt = "{:.2f}"
-            ss = fmt + ' +/- ' + fmt + ' [' + fmt + ', ' + fmt + ']\n'
-            print(ss.format(m, s, mn, mx))
+
+        mm = np.array(metrics) * 100
+        m = np.mean(mm)
+        s = np.std(mm)
+        mn = np.min(mm)
+        mx = np.max(mm)
+        fmt = "{:.2f}"
+        ss = fmt + ' +/- ' + fmt + ' [' + fmt + ', ' + fmt + ']\n'
+        print(ss.format(m, s, mn, mx))
 
