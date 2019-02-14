81b3948ffb93bbc58e4d5064d0545a50f7072a1b
swabhs <swabha.swayamdipta@gmail.com>
2019-02-14 12:23:38 -0800

    renaming files for merging

--- allennlp/models/label_encoder_seglm.py
+++ allennlp/models/label_encoder_seglm.py
@@ -18,49 +18,8 @@ from allennlp.nn import InitializerApplicator
 from allennlp.training.metrics.perplexity import Perplexity
 
 
-@Model.register('segmental_language_model')
-class SegmentalLanguageModel(LanguageModel):
-    """
-    The ``LanguageModel`` applies a "contextualizing"
-    ``Seq2SeqEncoder`` to uncontextualized embeddings, using a ``SoftmaxLoss``
-    module (defined above) to compute the language modeling loss.
-
-    If bidirectional is True,  the language model is trained to predict the next and
-    previous tokens for each token in the input. In this case, the contextualizer must
-    be bidirectional. If bidirectional is False, the language model is trained to only
-    predict the next token for each token in the input; the contextualizer should also
-    be unidirectional.
-
-    If your language model is bidirectional, it is IMPORTANT that your bidirectional
-    ``Seq2SeqEncoder`` contextualizer does not do any "peeking ahead". That is, for its
-    forward direction it should only consider embeddings at previous timesteps, and for
-    its backward direction only embeddings at subsequent timesteps. Similarly, if your
-    language model is unidirectional, the unidirectional contextualizer should only
-    consider embeddings at previous timesteps. If this condition is not met, your
-    language model is cheating.
-
-    Parameters
-    ----------
-    vocab: ``Vocabulary``
-    text_field_embedder: ``TextFieldEmbedder``
-        Used to embed the indexed tokens we get in ``forward``.
-    contextualizer: ``Seq2SeqEncoder``
-        Used to "contextualize" the embeddings. As described above,
-        this encoder must not cheat by peeking ahead.
-    dropout: ``float``, optional (default: None)
-        If specified, dropout is applied to the contextualized embeddings before computation of
-        the softmax. The contextualized embeddings themselves are returned without dropout.
-    num_samples: ``int``, optional (default: None)
-        If provided, the model will use ``SampledSoftmaxLoss``
-        with the specified number of samples. Otherwise, it will use
-        the full ``_SoftmaxLoss`` defined above.
-    sparse_embeddings: ``bool``, optional (default: False)
-        Passed on to ``SampledSoftmaxLoss`` if True.
-    bidirectional: ``bool``, optional (default: False)
-        Train a bidirectional language model, where the contextualizer
-        is used to predict the next and previous token for each input token.
-        This must match the bidirectionality of the contextualizer.
-    """
+@Model.register('label_encoder_seglm')
+class LabelEncoderSegLM(LanguageModel):
     def __init__(self,
                  vocab: Vocabulary,
                  text_field_embedder: TextFieldEmbedder,
@@ -124,18 +83,6 @@ class SegmentalLanguageModel(LanguageModel):
                 seg_ends: torch.Tensor,
                 seg_starts: torch.Tensor) -> Dict[str, torch.Tensor]:
         """
-        Computes the averaged forward (and backward, if language model is bidirectional)
-        LM loss from the batch.
-
-        By convention, the input dict is required to have at least a ``"tokens"``
-        entry that's the output of a ``SingleIdTokenIndexer``, which is used
-        to compute the language model targets.
-
-        Parameters
-        ----------
-        tokens: ``torch.Tensor``, required.
-            The output of ``Batch.as_tensor_dict()`` for a batch of sentences.
-
         Returns
         -------
         Dict with keys:
