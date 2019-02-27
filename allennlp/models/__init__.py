bd9f0c49caa928397a7436e1f25d45d1442e4643
swabhs <swabha.swayamdipta@gmail.com>
2019-02-26 18:06:05 -0800

    adding to model init

--- allennlp/models/__init__.py
+++ allennlp/models/__init__.py
@@ -30,4 +30,4 @@ from allennlp.models.bidirectional_lm import BidirectionalLanguageModel
 from allennlp.models.language_model import LanguageModel
 from allennlp.models.label_encoder_seglm import LabelEncoderSegLM
 from allennlp.models.segmental_language_model import SegmentalLanguageModel
-
+from allennlp.models.simple_classifier import SimpleClassifier
