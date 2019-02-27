45184ea8b1ad17e24b1fb00f0eaf2ea1c135a8a8
swabhs <swabha.swayamdipta@gmail.com>
2019-02-26 21:24:58 -0800

    working simplified bcn

--- allennlp/models/simple_classifier.py
+++ allennlp/models/simple_classifier.py
@@ -46,10 +46,6 @@ class SimpleClassifier(Model):
         If provided, will be used to concatenate pretrained ELMo representations to
         either the integrator output (``use_integrator_output_elmo``) or the
         input (``use_input_elmo``).
-    use_input_elmo : ``bool`` (default=``False``)
-        If true, concatenate pretrained ELMo representations to the input vectors.
-    use_integrator_output_elmo : ``bool`` (default=``False``)
-        If true, concatenate pretrained ELMo representations to the integrator output.
     initializer : ``InitializerApplicator``, optional (default=``InitializerApplicator()``)
         Used to initialize the model parameters.
     regularizer : ``RegularizerApplicator``, optional (default=``None``)
@@ -57,9 +53,13 @@ class SimpleClassifier(Model):
     """
     def __init__(self, vocab: Vocabulary,
                  text_field_embedder: TextFieldEmbedder,
+                 pre_encode_feedforward: FeedForward,
                  encoder: Seq2SeqEncoder,
+                 integrator: Seq2SeqEncoder,
+                 dropout: float,
+                 integrator_dropout: float,
+                 output_layer: Maxout, #Union[FeedForward, Maxout],
                  feedforward: Optional[FeedForward] = None,
-                 dropout: float = 0.0,
                  initializer: InitializerApplicator = InitializerApplicator(),
                  regularizer: Optional[RegularizerApplicator] = None) -> None:
         super().__init__(vocab, regularizer)
@@ -68,18 +68,41 @@ class SimpleClassifier(Model):
         self._dropout = torch.nn.Dropout(dropout)
         self._num_classes = self.vocab.get_vocab_size("labels")
 
+        self._pre_encode_feedforward = pre_encode_feedforward
         self._encoder = encoder
         self._feedforward = feedforward
+        self._integrator = integrator
+        self._integrator_dropout = torch.nn.Dropout(integrator_dropout)
 
         if feedforward is not None:
             output_dim = feedforward.get_output_dim()
         else:
             output_dim = self._encoder.get_output_dim()
-        self._output_layer = TimeDistributed(Linear(output_dim,
-                                                    self._num_classes))
 
-        check_dimensions_match(text_field_embedder.get_output_dim(), encoder.get_input_dim(),
-                               "text field embedding dim", "encoder input dim")
+        self._combined_integrator_output_dim = self._integrator.get_output_dim()
+        self._self_attentive_pooling_projection = Linear(self._combined_integrator_output_dim, 1)
+
+        self._output_layer = output_layer
+        check_dimensions_match(text_field_embedder.get_output_dim(),
+                               self._pre_encode_feedforward.get_input_dim(),
+                               "text field embedder output dim",
+                               "Pre-encoder feedforward input dim")
+        check_dimensions_match(self._pre_encode_feedforward.get_output_dim(),
+                               self._encoder.get_input_dim(),
+                               "Pre-encoder feedforward output dim",
+                               "Encoder input dim")
+        check_dimensions_match(self._encoder.get_output_dim() * 3,
+                               self._integrator.get_input_dim(),
+                               "Encoder output dim * 3",
+                               "Integrator input dim")
+        check_dimensions_match(self._integrator.get_output_dim() * 4,
+                               self._output_layer.get_input_dim(),
+                               "Integrator output dim * 4",
+                               "Output layer input dim")
+        check_dimensions_match(self._output_layer.get_output_dim(),
+                               self._num_classes,
+                               "Output layer output dim",
+                               "Number of classes.")
 
         self.metrics = {
                 "accuracy": CategoricalAccuracy(),
@@ -115,16 +138,45 @@ class SimpleClassifier(Model):
         if self._dropout:
             embedded_text_input = self._dropout(embedded_text_input)
 
-        encoded_text = self._encoder(embedded_text_input, mask)
+        pre_encoded_text = self._pre_encode_feedforward(embedded_text_input)
+        encoded_tokens = self._encoder(pre_encoded_text, mask)
 
         if self._dropout:
-            encoded_text = self._dropout(encoded_text)
+            encoded_tokens = self._dropout(encoded_tokens)
 
         if self._feedforward is not None:
-            encoded_text = self._feedforward(encoded_text)
+            encoded_tokens = self._feedforward(encoded_tokens)
 
-        logits = self._output_layer(encoded_text)
+        attention_logits = encoded_tokens.bmm(encoded_tokens.permute(0, 2, 1).contiguous())
+        attention_weights = util.masked_softmax(attention_logits, mask)
+        encoded_text = util.weighted_sum(encoded_tokens, attention_weights)
 
+        # Build the input to the integrator
+        integrator_input = torch.cat([encoded_tokens,
+                                      encoded_tokens - encoded_text,
+                                      encoded_tokens * encoded_text], 2)
+        integrated_encodings = self._integrator(integrator_input, mask)
+        # Simple Pooling layers
+        max_masked_integrated_encodings = util.replace_masked_values(
+                integrated_encodings, mask.unsqueeze(2), -1e7)
+        max_pool = torch.max(max_masked_integrated_encodings, 1)[0]
+        min_masked_integrated_encodings = util.replace_masked_values(
+                integrated_encodings, mask.unsqueeze(2), +1e7)
+        min_pool = torch.min(min_masked_integrated_encodings, 1)[0]
+        mean_pool = torch.sum(integrated_encodings, 1) / torch.sum(mask, 1, keepdim=True)
+
+        # Self-attentive pooling layer
+        # Run through linear projection. Shape: (batch_size, sequence length, 1)
+        # Then remove the last dimension to get the proper attention shape (batch_size, sequence length).
+        self_attentive_logits = self._self_attentive_pooling_projection(
+                integrated_encodings).squeeze(2)
+        self_weights = util.masked_softmax(self_attentive_logits, mask)
+        self_attentive_pool = util.weighted_sum(integrated_encodings, self_weights)
+
+        pooled_representations = torch.cat([max_pool, min_pool, mean_pool, self_attentive_pool], 1)
+        pooled_representations_dropped = self._integrator_dropout(pooled_representations)
+
+        logits = self._output_layer(pooled_representations_dropped)
         class_probabilities = torch.nn.functional.softmax(logits, dim=-1)
 
         output_dict = {'logits': logits, 'class_probabilities': class_probabilities}
--- training_config/sst/sst_simple_anlp.jsonnet
+++ training_config/sst/sst_simple_anlp.jsonnet
@@ -48,14 +48,36 @@
         },
       }
     },
-    "dropout": 0.1,
+    "dropout": 0.5,
+    "pre_encode_feedforward": {
+        "input_dim": 300+1024,
+        "num_layers": 1,
+        "hidden_dims": [300],
+        "activations": ["relu"],
+        "dropout": [0.25]
+    },
     "encoder": {
       "type": "lstm",
-      "input_size": 300+1024,
+      "input_size": 300,
       "hidden_size": 300,
       "num_layers": 2,
       "bidirectional": true
     },
+    "integrator": {
+      "type": "lstm",
+      "input_size": 1800,
+      "hidden_size": 300,
+      "num_layers": 1,
+      "bidirectional": true
+    },
+    "integrator_dropout": 0.1,
+    "output_layer": {
+        "input_dim": 2400,
+        "num_layers": 3,
+        "output_dims": [1200, 600, 5],
+        "pool_sizes": 4,
+        "dropout": [0.2, 0.3, 0.0]
+    }
   },
   "iterator": {
     "type": "bucket",
